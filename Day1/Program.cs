b726e7c8ee5f1b7a0d58a5ca03c27bb9e5a51c5b
David Wright <720482+dkwgit@users.noreply.github.com>
2020-12-01 10:00:36 -0600

    First Day 1 stuff, plus a library to support loading resources easily

---
+++ Day1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AOC2020Library.ResourceLoader.GetPuzzleInput("1"));
+        }
+    }
+}
