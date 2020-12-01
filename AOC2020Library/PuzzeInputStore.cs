04887ed8954cfdf45e3c405048ad515011c6187f
David Wright <720482+dkwgit@users.noreply.github.com>
2020-12-01 12:24:06 -0600

    Solved puzzle 1, day 1

---
+++ AOC2020Library/PuzzeInputStore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+
+namespace AOC2020Library
+{
+    public class PuzzleInputStore
+    {
+        public static List<string> GetPuzzleInputList(string dayNumber)
+        {
+            List<string> list = new List<string>(1000);
+            string input = GetPuzzleInput(dayNumber).Trim();
+
+            using (System.IO.StringReader reader = new System.IO.StringReader(input))
+            {
+                while (reader.Peek() != -1)
+                {
+                    string line = reader.ReadLine();
+                    if (line.Trim().Length > 0)
+                    {
+                        list.Add(line);
+                    }
+                }
+            }
+            return list;
+        }
+        public static string GetPuzzleInput(string dayNumber)
+        {
+            Type resources = typeof(Resources);
+            PropertyInfo propertyToGet = resources.GetProperty($"Day{dayNumber}_PuzzleInput", BindingFlags.Static | BindingFlags.NonPublic);
+            string input = propertyToGet.GetValue(null) as string;
+            return input;
+        }
+    }
+}
--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day1
 {
@@ -6,7 +7,25 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(AOC2020Library.ResourceLoader.GetPuzzleInput("1"));
+            var input = AOC2020Library.PuzzleInputStore.GetPuzzleInputList("1");
+            Dictionary<int, int> puzzleItems = new Dictionary<int, int>();
+
+            int turn = 1;
+            foreach (var item in input)
+            {
+                int number = int.Parse(item);
+                int pairedNumber = 2020 - number;
+                if (puzzleItems.ContainsKey(pairedNumber))
+                {
+                    Console.WriteLine($"Found on turn {turn}, pair is {number},{pairedNumber}, which multiplied equal: {number * pairedNumber}");
+                    break;
+                }
+                else
+                {
+                    puzzleItems.Add(number, 1);
+                }
+                turn++;
+            }
         }
     }
 }
