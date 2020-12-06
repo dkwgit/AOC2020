629159e95fc81701310c2ca2490f8db526e3c774
David Wright <720482+dkwgit@users.noreply.github.com>
2020-12-05 19:07:22 -0600

    Suppressed guidance around an unused variable, annoying to not be able to examine the variable without recompiling and adding it back in

--- Utilities/RegressionTesting.cs
+++ Utilities/RegressionTesting.cs
@@ -15,7 +15,11 @@
                     puzzle.RegressionTest();
                 }
             }
+#pragma warning disable CS0168 // Variable is declared but never used
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
             catch (Exception e)
+#pragma warning restore IDE0059 // Unnecessary assignment of a value
+#pragma warning restore CS0168 // Variable is declared but never used
             {
                 testsPass = false;
             }
