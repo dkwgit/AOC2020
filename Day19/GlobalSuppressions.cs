fa718f160e381febd5b444ff3a427463bc376667
David Wright <720482+dkwgit@users.noreply.github.com>
2020-12-24 17:15:37 -0700

    suppression bad warning when a parameter is discarded via _

---
+++ Day19/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification = "Message incorrect when discarding parameter via _", Scope = "member", Target = "~M:AOC2020.Day19.TerminalRule.Create(AOC2020.Day19.Puzzle,AOC2020.Day19.IAbstractRule,System.Int32,System.String)~AOC2020.Day19.TerminalRule")]
