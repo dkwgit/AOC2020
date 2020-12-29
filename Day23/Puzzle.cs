4c236e693f5f5b8527d6309611b0b06352496c46
David Wright <720482+dkwgit@users.noreply.github.com>
2020-12-29 15:22:42 -0700

    Day 23 Part 1

--- Day23/Puzzle.cs
+++ Day23/Puzzle.cs
@@ -1,6 +1,8 @@
 ﻿namespace AOC2020.Day23
 {
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
     using AOC2020.Utilities;
     using Microsoft.Extensions.Logging;
 
@@ -9,6 +11,8 @@
         private readonly ILogger _logger;
 
         private List<string> _input = null;
+
+        private List<int> _cups;
 
         public Puzzle(ILogger<Puzzle> logger)
         {
@@ -23,7 +27,18 @@
         {
             get
             {
-                string answer = string.Empty;
+                List<int> finalList = RunGame(_cups, 100);
+
+                int oneIndex = finalList.FindIndex(x => x == 1);
+                for (int i = 0; i < oneIndex; i++)
+                {
+                    finalList.Add(finalList[0]);
+                    finalList.RemoveAt(0);
+                }
+
+                Debug.Assert(finalList[0] == 1, "Expecting label 1 cup to be leftmost");
+                string answer = string.Join(string.Empty, finalList.Skip(1).Select(x => x.ToString()).ToList());
+
                 _logger.LogInformation("{Day}/Part1: Found {answer}", Day, answer);
                 return answer;
             }
@@ -39,9 +54,57 @@
             }
         }
 
+        public static List<int> RunGame(List<int> cupList, int rounds)
+        {
+            List<int> localList = new List<int>(cupList);
+
+            int currentCupIndex = 0;
+            List<int> removeCups = new ();
+
+            for (int i = 0; i < rounds; i++)
+            {
+                removeCups.AddRange(localList.GetRange(currentCupIndex + 1, 3));
+                localList.RemoveRange(currentCupIndex + 1, 3);
+                int min = localList.Min();
+                int max = localList.Max();
+                int value = localList[currentCupIndex] - 1;
+
+                bool looking = true;
+                int index = -1;
+
+                while (looking)
+                {
+                    if (value < min)
+                    {
+                        value = max;
+                    }
+
+                    index = localList.FindIndex(c => c == value);
+                    if (index == -1)
+                    {
+                        value -= 1;
+                    }
+                    else
+                    {
+                        looking = false;
+                        break;
+                    }
+                }
+
+                localList.InsertRange(index + 1, removeCups);
+                removeCups.Clear();
+                localList.Add(localList[0]);
+                localList.RemoveAt(0);
+            }
+
+            return localList;
+        }
+
         public void ProcessPuzzleInput(List<string> input)
         {
             _input = input;
+
+            _cups = _input[0].ToArray().Select(x => int.Parse(x.ToString())).ToList();
         }
     }
 }
--- Utilities/Resources.Designer.cs
+++ Utilities/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace AOC2020.Utilities {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -1483,7 +1483,7 @@ namespace AOC2020.Utilities {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to inputData
+        ///   Looks up a localized string similar to 215694783
         ///.
         /// </summary>
         internal static string Day23_PuzzleInput {
@@ -1493,7 +1493,7 @@ namespace AOC2020.Utilities {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to part1.
+        ///   Looks up a localized string similar to 46978532.
         /// </summary>
         internal static string Day23Part1_Answer {
             get {
