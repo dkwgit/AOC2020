8c51d63dfc0d46021e239d9fba8534b29fd09535
David Wright <720482+dkwgit@users.noreply.github.com>
2021-01-02 15:42:25 -0600

    Improved Day 14 by 30x

--- Day14Computer/Memory.cs
+++ Day14Computer/Memory.cs
@@ -6,9 +6,7 @@
 
     public class Memory : IMemory
     {
-        private readonly SortedList<long, int> _locations = new ();
-
-        private readonly LinkedList<MemoryWord> _wordList = new ();
+        private readonly SortedList<long, MemoryWord> _locations = new ();
 
         private int _wordWidth = -1;
 
@@ -24,46 +22,21 @@
 
         public MemoryWord GetWord(long location)
         {
-            if (_locations.ContainsKey(location))
+            if (!_locations.ContainsKey(location))
             {
-                var node = _wordList.Find(new MemoryWord(location));
-                return node.Value;
+                _locations.Add(location, CreateNewWord(location));
             }
-            else
-            {
-                var word = CreateNewWord(location);
 
-                if (_locations.Count == 0)
-                {
-                    _wordList.AddFirst(word);
-                }
-                else
-                {
-                    var result = _locations.Keys.Where(x => x > location).FirstOrDefault();
-                    if (result == 0)
-                    {
-                        _wordList.AddLast(word);
-                    }
-                    else
-                    {
-                        var node = _wordList.Find(new MemoryWord(result));
-                        _wordList.AddBefore(node, word);
-                    }
-                }
-
-                _locations.Add(location, 1);
-
-                return word;
-            }
+            return _locations[location];
         }
 
         public List<MemoryWord> GetMemory()
         {
             List<MemoryWord> words = new ();
 
-            foreach (var item in _wordList.AsEnumerable())
+            foreach (var key in _locations.Keys)
             {
-                words.Add(item);
+                words.Add(_locations[key]);
             }
 
             return words;
