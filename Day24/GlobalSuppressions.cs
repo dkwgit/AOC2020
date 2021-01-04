9f06e3cd841a871d3a0c55a5f18ae4fbff614e2a
David Wright <720482+dkwgit@users.noreply.github.com>
2021-01-04 00:28:15 -0600

    Suppression file

---
+++ Day24/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1025:Code should not contain multiple whitespace in a row", Justification = "Extra white space for tabular layout", Scope = "member", Target = "~M:AOC2020.Day24.TileManager.GetTileNumber(AOC2020.Day24.TileManager,System.Int32,System.Int32,System.Int32)~System.Int32")]
